f57cc7c5be7a47a52abe366193a849b1d8e3b365
Jair Carbajal <jair.carbajal91@gmail.com>
2022-10-05 22:26:45 -0700

    added loading animations to deleting tweets

--- react-app/src/components/DeleteComment/index.js
+++ react-app/src/components/DeleteComment/index.js
@@ -1,14 +1,21 @@
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { getAllTweetsThunk } from "../../store/tweets";
+import { useState, useEffect } from "react";
 import handleDeleteImage from "../../utils/DeleteImage";
-
+import ButtonLoadingAnimation from "../LoadingAnimation/ButtonLoadingAnimation";
 
 const DeleteComment = ({ setShowDeleteComment, comment }) => {
   const history = useHistory();
   const dispatch = useDispatch();
+  const [isDeleting, setIsDeleting] = useState(false)
+
+  useEffect(() => {
+    return () => setIsDeleting(false)
+  }, [])
 
   const handleDelete = async () => {
+    setIsDeleting(true)
     if (comment.comment_images.length) {
       await handleDeleteImage(dispatch, comment.comment_images[0].id, comment.comment_images[0].key)
     }
@@ -26,9 +33,13 @@ const DeleteComment = ({ setShowDeleteComment, comment }) => {
   return (
     <div className="delete-tweet container">
       <h2>Are you sure you want to delete this comment?</h2>
-      <div className="delete-tweet buttons container">
-        <button className='delete-tweet button no' onClick={() => setShowDeleteComment(false)}>Cancel</button>
-        <button className='delete-tweet button yes' onClick={handleDelete}>Delete</button>
+      <div className={isDeleting ? "delete-tweet buttons container loading" : "delete-tweet buttons container"}>
+        {isDeleting ? <ButtonLoadingAnimation /> :
+          <>
+            <button className='delete-tweet button no' onClick={() => setShowDeleteComment(false)}>Cancel</button>
+            <button className='delete-tweet button yes' onClick={handleDelete}>Delete</button>
+          </>
+        }
       </div>
     </div>
   )
